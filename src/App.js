13be9cac094a95eb74fbe590591980288f893a5f
dboehm <drdboehm@gmx.de>
2025-06-30 19:14:32 +0200

    App.js with changed routing

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ import {Users} from './users';
 export const App = () => (
     <BrowserRouter>
         <Routes>
-            {/*<Route exact path='/' element={<Navigate to='/bilder/pending' />} />*/}
-            <Route exact path='/' element={<Navigate to='/initial-page' />} />
+            <Route exact path='/' element={<Navigate to='/bilder/pending' />} />
+            {/*<Route exact path='/' element={<Navigate to='/initial-page' />} />*/}
             <Route exact path='/login' element={<Login />} />
             <Route exact path='/initial-page' element={<InitialPage />} />
             <Route exact path='/bilder/pending'
-                   element={<Bilder title='Bilder' filter={t => !Boolean(t.protect)} />} />
+                   element={<Bilder title='Bilder' filter={t => !Boolean(t.deleted)} />} />
             <Route exact path='/users' element={<Users />} />
         </Routes>
     </BrowserRouter>
