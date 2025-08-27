31facf0a4cb4a2e8969155606b08d34e40691e59
boehm-gipmbh <boehm@gipmbh.de>
2025-08-27 13:52:14 +0200

    ChangePasswordDialog wird mit Boolean - Konstante in UserIcon.js aus oder eingeblendet

--- src/layout/UserIcon.js
+++ src/layout/UserIcon.js
@@ -9,40 +9,43 @@ import {api} from '../users';
 import {openChangePassword} from './redux';
 
 export const UserIcon = () => {
-  const [anchorEl, setAnchorEl] = useState(null);
-  const menuOpen = Boolean(anchorEl);
-  const closeMenu = () => setAnchorEl(null);
-  const dispatch = useDispatch();
-  const {data} = api.endpoints.getSelf.useQuery();
-  return (
-    <>
-      <Tooltip title='Profile'>
-        <IconButton color='inherit' onClick={event => setAnchorEl(event.currentTarget)}>
-          <AccountCircleIcon />
-        </IconButton>
-      </Tooltip>
-      <Menu
-        anchorEl={anchorEl}
-        open={menuOpen}
-        onClose={closeMenu}
-      >
-        {data && <MenuItem>{data.name}</MenuItem>}
-        <MenuItem onClick={() => {
-          dispatch(openChangePassword());
-          closeMenu();
-        }}>
-          <ListItemIcon>
-            <KeyIcon />
-          </ListItemIcon>
-          Change Password
-        </MenuItem>
-        <MenuItem onClick={() => dispatch(logout())}>
-          <ListItemIcon>
-            <LogoutIcon />
-          </ListItemIcon>
-          Logout
-        </MenuItem>
-      </Menu>
-    </>
-  );
+    const [anchorEl, setAnchorEl] = useState(null);
+    const menuOpen = Boolean(anchorEl);
+    const closeMenu = () => setAnchorEl(null);
+    const dispatch = useDispatch();
+    const {data} = api.endpoints.getSelf.useQuery();
+    const SHOW_CHANGE_PASSWORD_BUTTON = false; // auf true setzen, um Buttons wieder anzuzeigen
+    return (
+        <>
+            <Tooltip title='Profile'>
+                <IconButton color='inherit' onClick={event => setAnchorEl(event.currentTarget)}>
+                    <AccountCircleIcon/>
+                </IconButton>
+            </Tooltip>
+            <Menu
+                anchorEl={anchorEl}
+                open={menuOpen}
+                onClose={closeMenu}
+            >
+                {data && <MenuItem>{data.name}</MenuItem>}
+                {SHOW_CHANGE_PASSWORD_BUTTON && (
+                    <MenuItem onClick={() => {
+                        dispatch(openChangePassword());
+                        closeMenu();
+                    }}>
+                        <ListItemIcon>
+                            <KeyIcon/>
+                        </ListItemIcon>
+                        Change Password
+                    </MenuItem>
+                )}
+                <MenuItem onClick={() => dispatch(logout())}>
+                    <ListItemIcon>
+                        <LogoutIcon/>
+                    </ListItemIcon>
+                    Logout
+                </MenuItem>
+            </Menu>
+        </>
+    );
 };
